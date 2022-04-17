cd1cce74658383891c4bbf7e158c5c7ccfbd36d9
loerk <fra.se@posteo.net>
2022-04-17 19:24:16 +0200

    added berlinWeather stats

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,5 +1,58 @@
 import React, { useEffect, useState } from "react";
 
-const weatherKey = '639d9251f29344c48515467b1b904768'
-let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=${part}&appid=${key}`
+const weatherKey = "639d9251f29344c48515467b1b904768";
+//let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=${part}&appid=${weatherKey}`;
 
+const Weather = () => {
+  const [temp, setTemp] = useState("");
+  const [feelTemp, setFeelTemp] = useState("");
+  const [hide, setHide] = useState(true);
+  const [city, setCity] = useState({ name: "Berlin", long: "", lat: "" });
+  const [alert, setAlert] = useState([]);
+
+  const getData = async () => {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=52.45&lon=13.43&units=metric&exclude=hourly,minutely&appid=${weatherKey}`
+    );
+    const data = await response.json();
+    console.log(data);
+    setTemp(data.current.temp);
+    let alertsArr = data.alerts;
+    setFeelTemp(data.current.feels_like);
+    alertsArr.forEach((item) => setAlert([item.description]));
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <div id="weather">
+      <div className={!city.name ? "hide" : null}>
+        <h2>The current Temperature in {city.name} is</h2>
+        <div className="currentTemp">
+          <h2>{temp} Celsius</h2>
+          <p>but actually feels like</p>
+        </div>
+        <button
+          onClick={() => {
+            hide ? setHide(false) : setHide(true);
+          }}
+        >
+          wanna know?
+        </button>
+        <h2 className={hide ? "hide" : null}>{feelTemp} Celsius</h2>
+      </div>
+      <div className="alertbox">
+        <p>
+          {" "}
+          {alert !== []
+            ? `Take care: ${alert}`
+            : "Currently no alerts for this region"}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Weather;
--- src/container/App.js
+++ src/container/App.js
@@ -5,19 +5,21 @@ import Bank from "../components/Bank";
 import Quote from "../components/Quote";
 import TodoList from "../components/TodoList";
 import Scroll from "../components/Scroll";
+import Weather from "../components/Weather";
 
 function App() {
-  const [display, setDisplay] = useState({ todo: false, quote: false, bank: false })
+  const [display, setDisplay] = useState({ todo: false, quote: false, bank: false, weather: false })
 
   const showToDo = () => {
-    setDisplay({ todo: true, quote: false, bank: false })
+    setDisplay({ todo: true, quote: false, bank: false, weather: false })
   };
   const showBank = () => {
-    setDisplay({ todo: false, quote: false, bank: true })
+    setDisplay({ todo: false, quote: false, bank: true, weather: false })
   };
   const showQuote = () => {
-    setDisplay({ todo: false, quote: true, bank: false })
+    setDisplay({ todo: false, quote: true, bank: false, weather: false })
   };
+  const showWeather = () => { setDisplay({ todo: false, quote: false, bank: false, weather: true }) }
 
 
 
@@ -32,15 +34,18 @@ function App() {
         <button onClick={showToDo} id="showToDo">
           create your ToDo-List
         </button>
-        <button onClick={showQuote} id="showDone">
+        <button onClick={showQuote} id="showQuote">
           get a Quote of the day
         </button>
-        <button onClick={showBank} id="showAll">
+        <button onClick={showBank} id="showBank">
           check your bank account
+        </button>
+        <button onClick={showWeather} id="showWeather">
+          check the weather
         </button>
       </div>
       <Scroll>
-        <main className={!display.todo && !display.quote && !display.bank ? "hide" : ""}>
+        <main className={!display.todo && !display.quote && !display.bank && !display.weather ? "hide" : ""}>
           <div className={!display.todo ? "section section-todo hide" : "section section-todo"}>
             <h2>Fill your To-Do List</h2>
             <TodoList />
@@ -50,6 +55,9 @@ function App() {
           </div>
           <div className={!display.bank ? "section section-bank hide" : "section section-bank"}>
             <Bank />
+          </div>
+          <div className={!display.weather ? "section section-weather hide" : "section section-weather"}>
+            <Weather />
           </div>
         </main>
       </Scroll>
