01ebc4516c6c20fd67ac5879655812bd06f13e7b
loerk <fra.se@posteo.net>
2022-05-07 17:36:07 +0200

    hide scrollbar

--- src/components/scrollContainer/ScrollContainer.js
+++ src/components/scrollContainer/ScrollContainer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function ScrollContainer({ children }) {
     return (
-        <div style={{ paddingLeft: "2rem", paddingRight: "2rem", marginY: "8rem", overflowY: 'scroll', height: '500px' }}>
+        <div className="scrollContainer">
             {children}
         </div>
     )
